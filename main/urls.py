6cbf197724f1cccb9d9cb0aaf992d8b91e21ddf1
Harikrishnan14 <harikrishnankhd09@gmail.com>
2023-10-31 18:42:35 +0530

    Fetching the Posts

--- main/urls.py
+++ main/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('questions/', views.QuestionListView.as_view(), name="questionList"),
 ]
--- main/views.py
+++ main/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Question
+
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+
+class QuestionListView(ListView):
+    model = Question
+    context_object_name = 'questions'
+    ordering = ['-date_created']
