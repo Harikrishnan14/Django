688379f45462e2ddb091cf2b002e7862e8240784
Harikrishnan14 <harikrishnankhd09@gmail.com>
2023-10-31 16:09:43 +0530

    Login & Logout Function Completed

--- users/views.py
+++ users/views.py
@@ -2,15 +2,18 @@ from django.shortcuts import render, redirect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib import messages
 from .forms import UserRegistrationForm
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
+
+@login_required
 def signup(request):
     if request.method == "POST":
         form = UserRegistrationForm(request.POST)
         if form.is_valid():
             form.save()
             messages.success(request, "Account Successfully Created, Login Now!")
-            return redirect("home")
+            return redirect("login")
     else:
         form = UserRegistrationForm()
     return render(request, 'signup.html', {'form':form})
