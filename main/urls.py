98e4474fdcf7d2ae3f950c0f1b6939ddf2f23d4a
Harikrishnan14 <harikrishnankhd09@gmail.com>
2023-11-01 02:41:40 +0530

    Update and Delete Functionality & Some User Restrictions

--- main/urls.py
+++ main/urls.py
@@ -6,4 +6,7 @@ urlpatterns = [
     path('questions/', views.QuestionListView.as_view(), name="questionList"),
     path('questions/<int:pk>/', views.QuestionDetailView.as_view(), name="questionDetail"),
     path('questions/new/', views.QuestionCreateView.as_view(), name="questionCreate"),
+    path('questions/<int:pk>/update/', views.QuestionUpdateView.as_view(), name="questionUpdate"),
+    path('questions/<int:pk>/delete/', views.QuestionDeleteView.as_view(), name="questionDelete"),
+    
 ]
--- main/views.py
+++ main/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Question
+from django.contrib.auth.mixins import UserPassesTestMixin, LoginRequiredMixin
 
 
 # Create your views here.
@@ -16,10 +17,32 @@ class QuestionListView(ListView):
 class QuestionDetailView(DetailView):
     model = Question
 
-class QuestionCreateView(CreateView):
+class QuestionCreateView(LoginRequiredMixin, CreateView):
     model = Question
     fields = ['title', 'content']
 
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+    
+class QuestionUpdateView(UserPassesTestMixin, LoginRequiredMixin, UpdateView):
+    model = Question
+    fields = ['title', 'content']
+
+    def test_func(self):
+        question = self.get_object()
+        if self.request.user == question.user:
+            return True
+        else:
+            return False
+        
+class QuestionDeleteView(UserPassesTestMixin, LoginRequiredMixin, DeleteView):
+    model = Question
+    context_object_name = 'questions'
+    success_url = "/questions"
+    def test_func(self):
+        question = self.get_object()
+        if self.request.user == question.user:
+            return True
+        else:
+            return False
